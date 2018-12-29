924138692ab871dd6c7fc6aba12a760426d12be8
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-12-29 21:28:18 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # npxme
+
 Show your npx card on the web
+
+# Usage
+
+```https://npxme.now.sh/:your-card-id```
+
+[https://npxme.now.sh/zrrrzzt](https://npxme.now.sh/zrrrzzt)
+
+# License
+
+[MIT](LICENSE)
---
+++ index.js
@@ -0,0 +1,45 @@
+const { readFileSync } = require('fs')
+const md = require('markdown-it')()
+const { send } = require('micro')
+const Router = require('router')
+const finalhandler = require('finalhandler')
+const cors = require('cors')
+const axios = require('axios')
+
+// Initialize a new router
+const router = Router()
+
+// CORS
+router.use(cors())
+
+// Handlers
+const getFrontpage = async (request, response) => {
+  const readme = await readFileSync(`${__dirname}/README.md`, 'utf-8')
+  send(response, 200, md.render(readme))
+}
+
+const getCard = async (request, response) => {
+  const { card } = request.params
+  const url = `https://unpkg.com/${card}/bin/output`
+  try {
+    const { data } = await axios.get(url)
+    response.setHeader('Content-Type', 'text/plain; charset=utf-8')
+    send(response, 200, data)
+  } catch (error) {
+    console.error(error)
+    send(response, 404, `Could not find card for ${card}`)
+  }
+}
+
+const getFavicon = async (request, response) => {
+  send(response, 404, 'not found')
+}
+
+// ROUTES
+router.get('/', getFrontpage)
+router.get('/favicon.ico', getFavicon)
+router.get('/:card', getCard)
+
+module.exports = (request, response) => {
+  router(request, response, finalhandler(request, response))
+}
